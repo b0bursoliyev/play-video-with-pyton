142eca9519356adb3b967a43181b361bdc3108ab
Soliyev <123302139+b0bursoliyev@users.noreply.github.com>
2023-10-05 12:33:53 +0500

    Update video.py

--- video.py
+++ video.py
@@ -1,7 +1,11 @@
 import cv2
+import glob
 import numpy as np
+#if you are opening your device's camera, you use cv2.VideoCapture(0)
 video = cv2.VideoCapture(0)
-
+#if you are opening your video in the path, you use way to your video
+path = 'your_path/video'
+video = glob.glob(path)
 while video.isOpened():
     _,rasm = video.read()
     cv2.imshow("Video",rasm)
